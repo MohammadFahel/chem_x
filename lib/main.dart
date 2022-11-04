384a82c40dbf1eb9019bdb1b96bad126d7d4ab95
Mohammad Fahel <201910094@students.asu.edu.jo>
2022-11-04 23:30:17 +0300

    Graduation Project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ChemX());
+}
+
+class ChemX extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ChemX_HomePage extends StatefulWidget {
+  @override
+  _ChemX_HomePageState createState() => _ChemX_HomePageState();
+}
+
+class _ChemX_HomePageState extends State<ChemX_HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
